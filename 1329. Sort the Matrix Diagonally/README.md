c85bfdceea0cc9aaefb39caea99bcadc59fc0702
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2022-08-28 23:27:55 +0200

    Today's problem Sun 28 Aug 2022 11:28:40 PM EET

---
+++ 1329. Sort the Matrix Diagonally/README.md
@@ -0,0 +1,3 @@
+# 1329. Sort the Matrix Diagonally
+
+Problem Link: https://leetcode.com/problems/sort-the-matrix-diagonally/
---
+++ 1329. Sort the Matrix Diagonally/main.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<vector<int>> diagonalSort(vector<vector<int>> &mat)
+  {
+    int rows = mat.size(), columns = mat[0].size();
+    map<int, priority_queue<int, vector<int>, greater<int>>> mpq;
+    for (int i = 0; i < rows; i++)
+    {
+      for (int j = 0; j < columns; j++)
+      {
+        mpq[i - j].push(mat[i][j]);
+      }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+      for (int j = 0; j < columns; j++)
+      {
+        mat[i][j] = mpq[i - j].top();
+        mpq[i - j].pop();
+      }
+    }
+    return mat;
+  }
+};
+
+int main()
+{
+  Solution obj;
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> mat(m);
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      int val;
+      cin >> val;
+      mat[i].push_back(val);
+    }
+  }
+  for (int i = 0; i < obj.diagonalSort(mat).size(); i++)
+  {
+    for (int j = 0; j < obj.diagonalSort(mat).size(); j++)
+    {
+      cout << obj.diagonalSort(mat)[i][j] << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
