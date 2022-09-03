e9ed78c17a1b57e9f948d8749af5c97fca34aad7
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2022-09-03 22:28:40 +0200

    Fri 02 Sep 2022 10:48:59 PM EET

--- 637. Average of Levels in Binary Tree/main.cpp
+++ 637. Average of Levels in Binary Tree/main.cpp
@@ -1,17 +1,3 @@
-#include "bits/stdc++.h"
-
-using namespace std;
-
-struct TreeNode
-{
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution
 {
 public:
@@ -39,6 +25,3 @@ public:
     return ans;
   }
 };
-int main()
-{
-}
