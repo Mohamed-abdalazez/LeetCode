5fa876edda87331770379dffab65332e8b107270
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2022-09-05 08:25:15 +0200

    Mon 05 Sep 2022 08:22:40 AM EET

--- 429. N-ary Tree Level Order Traversal/main.cpp
+++ 429. N-ary Tree Level Order Traversal/main.cpp
@@ -1,27 +1,3 @@
-#include "bits/stdc++.h"
-
-using namespace std;
-
-class Node
-{
-public:
-  int val;
-  vector<Node *> children;
-
-  Node() {}
-
-  Node(int _val)
-  {
-    val = _val;
-  }
-
-  Node(int _val, vector<Node *> _children)
-  {
-    val = _val;
-    children = _children;
-  }
-};
-
 class Solution
 {
 public:
